d7082f0ce5904dda72bf67810eefd8707356052f
Johnwat92 <JohnWat92@gmail.com>
2017-02-26 20:58:33 -0800

    updated about page

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -13,12 +13,14 @@ get_header(); ?>
 	                  <header class="entry-header">
 		                  <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	                  </header><!-- .entry-header -->
+      <div class= "belowHero">
             <div class = "container">
                   <h2>Our Story</h2>
                   <?php echo CFS()->get('about_our_story');?>
                   <h2>Our Team</h2>
                   <?php echo CFS()->get('about_our_team');?>
             </div>
+      </div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
